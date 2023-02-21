d8eec2f267e7016f49bcd56e3949fa833b3cf370
Mannan Shihab <amannanshihab@gmail.com>
2023-02-21 19:16:42 +0600

    upload

---
+++ resources/views/home/hero.blade.php
@@ -0,0 +1,15 @@
+<h3 class="mt-16 flex justify-center text-center text-4xl text-white font-bold">
+    Gohira A. J. Y. M. S. High School
+</h3>
+<!-- hero-section -->
+<div class="mt-16">
+<div class="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-2 gap-6 lg:gap-8">
+    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+        <img src="{{asset('home')}}/img/GHS-day-view.jpg" alt=""> 
+    </div>
+    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+        <img src="{{asset('home')}}/img/GHS-Night-view.jpg" alt=""> 
+    </div>
+</div>
+<img src="" alt="">
+</div><!--/ hero-section -->
---
+++ resources/views/home/member.blade.php
@@ -0,0 +1,72 @@
+<!-- Member -->
+<div class="mt-16">
+                <h3 class="mt-16 mb-10 flex justify-center text-center text-4xl text-white font-bold">
+                    Register Member
+                </h3>
+                <div class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-6 lg:gap-8">
+                    <!-- profile -->
+                    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <!-- User img -->
+                            <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                <img src="{{asset('home')}}/img/author.png" alt="">
+                            </div>
+                            <!-- User name -->
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Hasan</h2>
+                            <!-- User short intro -->
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas debitis distinctio eligendi veritatis beatae nemo ab....
+                            </p>
+                        </div>
+                    </div><!--/end profile -->
+                    
+                    <!-- profile -->
+                    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <!-- User img -->
+                            <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                <img src="{{asset('home')}}/img/author.png" alt="">
+                            </div>
+                            <!-- User name -->
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Noyem</h2>
+                            <!-- User short intro -->
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas debitis distinctio eligendi veritatis beatae nemo ab....
+                            </p>
+                        </div>
+                    </div><!--/end profile -->
+
+                    <!-- profile -->
+                    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <!-- User img -->
+                            <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                <img src="{{asset('home')}}/img/author.png" alt="">
+                            </div>
+                            <!-- User name -->
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Noyon</h2>
+                            <!-- User short intro -->
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas debitis distinctio eligendi veritatis beatae nemo ab....
+                            </p>
+                        </div>
+                    </div><!--/end profile -->
+                    
+                    <!-- profile -->
+                    <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <!-- User img -->
+                            <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                <img src="{{asset('home')}}/img/author.png" alt="">
+                            </div>
+                            <!-- User name -->
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</h2>
+                            <!-- User short intro -->
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas debitis distinctio eligendi veritatis beatae nemo ab....
+                            </p>
+                        </div>
+                    </div><!--/end profile -->
+                    
+                </div>
+            </div><!--/end Member -->  
